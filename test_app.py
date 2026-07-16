dd07b0a8641c47d187e7abbacc97fca9d68f0823
montassar892 <60817081+montassar892@users.noreply.github.com>
2026-07-16 10:07:55 +0100

    Update file

--- test_app.py
+++ test_app.py
@@ -1,23 +1,21 @@
 from app import app
 
+
 def test_health():
     client = app.test_client()
     response = client.get("/health")
-    
     assert response.status_code == 200
     assert response.get_json()["status"] == "ok"
+
 
 def test_add():
     client = app.test_client()
     response = client.get("/add/2/3")
-    
     assert response.get_json()["result"] == 5
+
 
 def test_add_negative():
     client = app.test_client()
     response = client.get("/add/-5/10")
-    
     assert response.get_json()["result"] == 5
-    
-      
 
